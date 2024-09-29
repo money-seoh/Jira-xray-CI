80fb788f2a6e565cb005409095b8df0efba377dd
Shubham <thakurshubham546@gmail.com>
2024-09-30 00:11:43 +0530

    added html extenstion

--- jira-xray-CI/src/test/java/com/moneyseoh/automation/runners/TestRunner.java
+++ jira-xray-CI/src/test/java/com/moneyseoh/automation/runners/TestRunner.java
@@ -8,7 +8,7 @@ import io.cucumber.junit.CucumberOptions;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = {"com/moneyseoh/automation/stepDefinitions"},
-        plugin = {"pretty", "html:target/cucumber-reports"},
+        plugin = {"pretty", "html:target/cucumber-reports.html"},
         monochrome = true
 )
 public class TestRunner {
